f9918b5ea0ddff8d54a07c9ddc663b21611176cd
Daniel Dương <daniel.duong@outlook.com>
2024-02-23 17:54:54 -0300

    English and French versions

--- deploy.py
+++ deploy.py
@@ -2,17 +2,24 @@
 """
 Upload index.html to using the FTP server.
 """
+from concurrent.futures import ThreadPoolExecutor
 from ftplib import FTP
 
 import os
 import os.path
 
-
+os.chdir("src")
 with open(os.path.join(os.environ["HOME"], ".qteam")) as f1:
     host = f1.readline()[:-1]
     user = f1.readline()[:-1]
     password = f1.readline()[:-1]
 
-    with FTP(host, user, password) as ftp, open("src/index.html", "rb") as f2:
-        ftp.cwd("www")
-        ftp.storbinary("STOR index.html", f2)
+    def deploy(path: str):
+        with FTP(host, user, password) as ftp, open(path, "rb") as f2:
+            ftp.cwd("www")
+            ftp.storbinary(f"STOR {path}", f2)
+
+    with ThreadPoolExecutor() as executor:
+        executor.submit(deploy, "index.html")
+        executor.submit(deploy, "en.html")
+        executor.submit(deploy, "styles.css")
