4d02dadfc7df0be74f098585bae63aa54d55c333
Daniel Dương <daniel.duong@outlook.com>
2024-02-25 14:42:44 -0300

    Start thread work

--- deploy.py
+++ deploy.py
@@ -23,28 +23,47 @@ except FileExistsError:
         user = f.readline()[:-1]
         password = f.readline()[:-1]
 
-os.chdir("src")
-with ftplib.FTP(host, user, password) as ftp:
-    ftp.set_debuglevel(1)
-    ftp.cwd("www")
 
-    def deploy():
-        with os.scandir(".") as it:
-            for entry in it:
-                if entry.is_dir():
-                    try:
-                        ftp.cwd(entry.name)
-                    except ftplib.error_perm as reason:
-                        if str(reason)[:3] == '550':
-                            ftp.mkd(entry.name)
-                            ftp.cwd(entry.name)
-                        else:
-                            raise
-                    os.chdir(entry.name)
-                    deploy()
-                    os.chdir("..")
-                    ftp.cwd("..")
-                else:
-                    with open(entry.name, "rb") as f2:
-                        ftp.storbinary(f"STOR {entry.name}", f2)
-    deploy()
+def cd(ftp: ftplib.FTP, path: str):
+    dirs = path.split(os.sep)
+    for d in dirs:
+        try:
+            ftp.cwd(d)
+        except ftplib.error_perm as reason:
+            if str(reason)[:3] == '550':
+                ftp.mkd(d)
+                ftp.cwd(d)
+            else:
+                raise
+
+
+def mkdir(path: str):
+    with ftplib.FTP(host, user, password) as ftp:
+        ftp.set_debuglevel(1)
+        head, tail = os.path.split(path)
+        cd(ftp, head)
+        try:
+            ftp.mkd(tail)
+        except ftplib.error_perm as reason:
+            if str(reason)[:3] != '550':
+                raise
+
+
+def put(path: str):
+    with ftplib.FTP(host, user, password) as ftp:
+        ftp.set_debuglevel(1)
+        head, tail = os.path.split(path)
+        cd(ftp, head)
+        with open(path, "rb") as f:
+            ftp.storbinary(f"STOR {tail}", f)
+
+
+def deploy(path: str):
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_dir():
+                mkdir(entry.path)
+                deploy(entry.path)
+            else:
+                put(entry.path)
+deploy("www")
