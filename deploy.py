820db7176285a6265ebcbac477bfa32defef51a7
Daniel Dương <daniel.duong@outlook.com>
2024-02-25 14:01:44 -0300

    Use context manager as recommended in the documentation

--- deploy.py
+++ deploy.py
@@ -29,21 +29,22 @@ with ftplib.FTP(host, user, password) as ftp:
     ftp.cwd("www")
 
     def deploy():
-        for entry in os.scandir("."):
-            if entry.is_dir():
-                try:
-                    ftp.cwd(entry.name)
-                except ftplib.error_perm as reason:
-                    if str(reason)[:3] == '550':
-                        ftp.mkd(entry.name)
+        with os.scandir(".") as it:
+            for entry in it:
+                if entry.is_dir():
+                    try:
                         ftp.cwd(entry.name)
-                    else:
-                        raise
-                os.chdir(entry.name)
-                deploy()
-                os.chdir("..")
-                ftp.cwd("..")
-            else:
-                with open(entry.name, "rb") as f2:
-                    ftp.storbinary(f"STOR {entry.name}", f2)
+                    except ftplib.error_perm as reason:
+                        if str(reason)[:3] == '550':
+                            ftp.mkd(entry.name)
+                            ftp.cwd(entry.name)
+                        else:
+                            raise
+                    os.chdir(entry.name)
+                    deploy()
+                    os.chdir("..")
+                    ftp.cwd("..")
+                else:
+                    with open(entry.name, "rb") as f2:
+                        ftp.storbinary(f"STOR {entry.name}", f2)
     deploy()
