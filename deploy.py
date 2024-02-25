a65f4ea9828a5a576863ae4d3a5f6d9402c84a66
Daniel Dương <daniel.duong@outlook.com>
2024-02-25 09:18:23 -0300

    Remove storecredentials.py

--- deploy.py
+++ deploy.py
@@ -2,36 +2,48 @@
 """
 Deploy using the FTP server.
 """
+import getpass
 import ftplib
 import os
 import os.path
 
+credentials_path = os.path.join(os.environ["HOME"], ".qteam")
+
+# Try to create ~/.qteam
+os.umask(0o77)
+try:
+    with open(credentials_path, "x") as f:
+        host = input("Host: ")
+        user = input("User: ")
+        password = getpass.getpass()
+        f.write(f"{host}\n{user}\n{password}\n")
+except FileExistsError:
+    with open(credentials_path) as f:
+        host = f.readline()[:-1]
+        user = f.readline()[:-1]
+        password = f.readline()[:-1]
+
 os.chdir("src")
-with open(os.path.join(os.environ["HOME"], ".qteam")) as f1:
-    host = f1.readline()[:-1]
-    user = f1.readline()[:-1]
-    password = f1.readline()[:-1]
+with ftplib.FTP(host, user, password) as ftp:
+    ftp.set_debuglevel(1)
+    ftp.cwd("www")
 
-    with ftplib.FTP(host, user, password) as ftp:
-        ftp.set_debuglevel(1)
-        ftp.cwd("www")
-
-        def deploy():
-            for entry in os.scandir("."):
-                if entry.is_dir():
-                    try:
+    def deploy():
+        for entry in os.scandir("."):
+            if entry.is_dir():
+                try:
+                    ftp.cwd(entry.name)
+                except ftplib.error_perm as reason:
+                    if str(reason)[:3] == '550':
+                        ftp.mkd(entry.name)
                         ftp.cwd(entry.name)
-                    except ftplib.error_perm as reason:
-                        if str(reason)[:3] == '550':
-                            ftp.mkd(entry.name)
-                            ftp.cwd(entry.name)
-                        else:
-                            raise
-                    os.chdir(entry.name)
-                    deploy()
-                    os.chdir("..")
-                    ftp.cwd("..")
-                else:
-                    with open(entry.name, "rb") as f2:
-                        ftp.storbinary(f"STOR {entry.name}", f2)
-        deploy()
+                    else:
+                        raise
+                os.chdir(entry.name)
+                deploy()
+                os.chdir("..")
+                ftp.cwd("..")
+            else:
+                with open(entry.name, "rb") as f2:
+                    ftp.storbinary(f"STOR {entry.name}", f2)
+    deploy()
